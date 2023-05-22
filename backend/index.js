5065a0196856123b1d0bdf165a184dde701900b0
Haruka <hfujii1013@gmail.com>
2023-05-22 16:31:27 -0700

    added router on backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { db } = require("./db/db");
+const { readdirSync } = require("fs");
+const { route } = require("./routes/money.router");
 const app = express();
 
 require("dotenv").config();
@@ -11,6 +13,11 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
+//routes
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
+
 const server = () => {
   db();
   app.listen(PORT, () => {
@@ -19,3 +26,18 @@ const server = () => {
 };
 
 server();
+
+//memo1
+//readdirSync("./routes") reads the contents of the ./routes directory synchronously.
+//It returns an array of file names present in the directory.
+
+//.map((route) => ...) is called on the array of file names returned by readdirSync.
+//This iterates over each file name and executes the provided callback function for each element.
+
+//app.use("/api/v1", require("./routes/" + route)) is the callback function executed for each file name. It registers a route handler to the Express application.
+
+//app.use() is a method in Express that registers middleware or route handlers.
+//"/api/v1" is the base path prefix for the routes.
+//require("./routes/" + route) dynamically imports the route file based on the current route file name being processed.
+//So, for each route file found in the ./routes directory, a route handler is registered
+//with the base path of "/api/v1" and the corresponding route file is imported.
