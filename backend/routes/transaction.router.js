a3dc10f43fd2fa54ed281d05d05f1aa84f900a2f
Haruka <hfujii1013@gmail.com>
2023-05-23 00:37:55 -0700

    added expense controller

--- backend/routes/transaction.router.js
+++ backend/routes/transaction.router.js
@@ -3,12 +3,20 @@ const {
   getIncomes,
   deleteIncome,
 } = require("../controllers/income.controller");
+const {
+  addExpense,
+  getExpense,
+  deleteExpense,
+} = require("../controllers/expense.controller");
 const router = require("express").Router();
 
 router
   .post("/add-income", addIncome)
   .get("/get-incomes", getIncomes)
-  .delete("/delete-income/:id", deleteIncome);
+  .delete("/delete-income/:id", deleteIncome)
+  .post("/add-expense", addExpense)
+  .get("/get-expenses", getExpense)
+  .delete("/delete-expense/:id", deleteExpense);
 module.exports = router;
 
 //This code defines a POST route for the path "/add-income" using router.post().
