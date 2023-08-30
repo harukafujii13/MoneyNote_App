eb31021255d3d4d4807372bfde2f07ba60d45824
Haruka <hfujii1013@gmail.com>
2023-08-29 23:09:54 -0700

    delete: colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,18 +8,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        'primay-orange': '#fb923c',
-        'primay-lavender': '#E1E5F2',
+        'primary-pink': '#C64191',
+        'primary-orange': '#fb923c',
+        'primary-lavender': '#E1E5F2',
         'primary-columbiaBlue': '#BFDBF7',
         'primary-teal': '#1F7A8C',
         'primary-gunmetal': '#22225F',
-        'primary-color': '#222260',
-        'primary-color2': 'rgba(34, 34, 96, .6)',
-        'primary-color3': 'rgba(34, 34, 96, .4)',
-        'color-green': '#42AD00',
-        'color-grey': '#aaa',
-        'color-accent': '#F56692',
-        'color-delete': '#FF0000',
       },
       fontFamily: {
         nunito: ['Nunito', 'sans-serif'],
