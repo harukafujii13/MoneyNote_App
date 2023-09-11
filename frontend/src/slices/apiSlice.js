6890b2c5e8f89dbb37bcc460188a3fa702290e1f
Haruka <hfujii1013@gmail.com>
2023-09-11 11:07:42 -0700

    fix: baseUrl

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // const baseQuery = fetchBaseQuery({ baseUrl: '' });
 const baseQuery = fetchBaseQuery({
   // baseUrl: '',
-  baseUrl: `${import.meta.env.VITE_BACKEND_URL}/api/v1/`,
+  baseUrl: 'https://moneynoteapp-production.up.railway.app',
   credentials: 'include',
 });
 
