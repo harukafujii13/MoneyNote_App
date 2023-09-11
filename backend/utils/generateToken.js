6d595e358bb1df015e9317b665d990dda4fb4256
Haruka <hfujii1013@gmail.com>
2023-09-11 11:36:47 -0700

    update: httpOnly

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,7 @@ const generateToken = (res, userId) => {
   });
 
   res.cookie('jwt', token, {
-    httpOnly: true,
+    httpOnly: process.env.NODE_ENV !== 'development' ? false : true,
     secure: process.env.NODE_ENV !== 'development',
     sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'none',
     maxAge: 30 * 24 * 60 * 60 * 1000, //30days
