6ceab5b34e12b7ed1d952324c8afbbf0ac789425
Haruka <hfujii1013@gmail.com>
2023-09-13 11:11:53 -0700

    remove: frontend static files code

--- backend/index.js
+++ backend/index.js
@@ -38,18 +38,18 @@ readdirSync('./routes').map((route) => {
 });
 
 // For production, serve frontend static files
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+// if (process.env.NODE_ENV === 'production') {
+//   const __dirname = path.resolve();
+//   app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+//   );
+// } else {
+//   app.get('/', (req, res) => {
+//     res.send('API is running....');
+//   });
+// }
 
 // Error Middlewares
 app.use(notFound);
